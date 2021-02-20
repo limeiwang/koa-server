71b673b20070a4e1214ae5efae46b7dd70201dba
李美旺 <v_limeiwang@duxiaoman.com>
2021-02-20 16:28:10 +0800

    build: koa-server

---
+++ config/default.js
@@ -0,0 +1,18 @@
+// default.js
+// 设置配置文件
+const config = {
+    // 启动端口
+    port: 3000,
+  
+    // 数据库配置
+    database: {
+      DATABASE: 'ceshi',
+      USERNAME: 'root',
+      PASSWORD: 'Li010158',
+      PORT: '3306',
+      HOST: 'localhost'
+    }
+  }
+  
+  module.exports = config
+ 
